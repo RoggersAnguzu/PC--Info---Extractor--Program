35f17688e26f5c36da243a1440208c6d8e4017bf
Roggers Anguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2023-09-12 23:38:38 +0300

    Add files via upload
    PC information Generator

---
+++ PC_Information_Generator/PC_Information_Generator/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace PC_Information_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.Location = new System.Drawing.Point(36, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get PC Name";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(272, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Location = new System.Drawing.Point(36, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Get Login User Name";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(272, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(373, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MintCream;
+            this.button3.Location = new System.Drawing.Point(36, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Get CPU Count";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(272, 181);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(373, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MintCream;
+            this.button4.Location = new System.Drawing.Point(36, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(197, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Get Up Time";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox4.Location = new System.Drawing.Point(272, 258);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(373, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MintCream;
+            this.button5.Location = new System.Drawing.Point(36, 308);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(197, 34);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Get Resolution";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox5.Location = new System.Drawing.Point(272, 320);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(373, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox6.Location = new System.Drawing.Point(272, 395);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(373, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MintCream;
+            this.button6.Location = new System.Drawing.Point(36, 383);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(197, 34);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Get Company";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(679, 445);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PC_IFORMATION_DEFINER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ PC_Information_Generator/PC_Information_Generator/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_Information_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Page Loading!, Would you like to Continue","Alert",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                MessageBox.Show("Continue with Checking your PC Checkings!");
+            }
+            else
+            {
+                MessageBox.Show("Thanks for using the Application");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = Environment.MachineName;
+            MessageBox.Show("The Detail in the box is Your Pc Name");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text= Environment.UserName;
+            MessageBox.Show("This is your Login name");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox3.Text = Environment.ProcessorCount.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int x;int j;
+            x = Environment.TickCount;
+            j = x / 1000; int y = j / 3600;//This changes the seconds to hours
+            this.textBox4.Text = y.ToString();
+            MessageBox.Show($"It is {y} hours from the Last Boot");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.textBox5.Text = System.Windows.Forms.Screen.PrimaryScreen.Bounds.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.textBox6.Text = Application.ProductVersion;
+        }
+    }
+}
